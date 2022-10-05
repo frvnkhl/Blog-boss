dec1a3f0414a13736ede0cb6af6b729ff5cb0c98
Frank Uhal <franquito@Franks-Mac-mini.local>
2022-10-05 15:51:07 +0200

    set loading state for loging

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { FacebookRounded, Google, Visibility, VisibilityOff } from "@mui/icons-material";
-import { FormControl, TextField, Input, InputLabel, InputAdornment, IconButton, Box, Button, Alert } from "@mui/material";
+import { FormControl, TextField, Input, InputLabel, InputAdornment, IconButton, Box, Button, Alert, CircularProgress } from "@mui/material";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import DataService from "../services/DataService";
@@ -14,6 +14,7 @@ const LoginForm = () => {
         colour: ''
     });
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     const handleChange = (event) => {
@@ -33,18 +34,21 @@ const LoginForm = () => {
     };
 
     const handleUserSubmit = async (event) => {
+        setLoading(true);
         DataService.loginUser(login).then(res => {
-            console.log({response: res});
+            console.log({ response: res });
             setMessage({
                 message: res.message,
                 colour: 'success'
             });
+            setLoading(false);
             router.reload();
         }).catch(err => {
             setMessage({
                 message: err.response.data.message,
                 colour: 'error'
             });
+            setLoading(false);
         });
         setLogin({
             email: '',
@@ -55,46 +59,56 @@ const LoginForm = () => {
 
     const handleGoogleSignIn = async () => {
         router.push('https://blog-boss-api.onrender.com/user/auth/google');
-    };  
-    
+    };
+
     const handleFacebookSignIn = async () => {
         router.push('https://blog-boss-api.onrender.com/user/auth/facebook');
     };
 
     return (
-        <Box sx={{ width: {xs:'95%', md:'40%'}, m: {xs: 1, md:5}, p: 2, display: 'grid', textAlign: 'center', boxShadow: 2 }}>
+        <Box sx={{ width: { xs: '95%', md: '40%' }, m: { xs: 1, md: 5 }, p: 2, display: 'grid', textAlign: 'center', boxShadow: 2 }}>
             {message.message !== '' && <Alert severity={message.colour}>{message.message}</Alert>}
             <h2>Login to your account</h2>
-            <FormControl sx={{ m: 1, }} variant="standard">
-                <TextField label="Username" variant="standard" name="username" value={login.username} onChange={handleChange} />
-            </FormControl>
-            <FormControl sx={{ m: 1, }} variant="standard">
-                <InputLabel htmlFor="login-password">Password</InputLabel>
-                <Input
-                    id="login-password"
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    value={login.password}
-                    onChange={handleChange}
-                />
-            </FormControl>
-            <Button variant="contained" sx={{ m: 1 }} color="success" onClick={handleUserSubmit}>Login</Button>
-            <Button variant="contained" sx={{ m: 1 }} color="primary" onClick={handleGoogleSignIn} startIcon={<Google />}>Login with Google</Button>
-            <Button variant="contained" sx={{
-                m: 1,
-                backgroundColor: '#4267B2'
-            }} onClick={handleFacebookSignIn} startIcon={<FacebookRounded />}>Login with Facebook</Button>
+            {
+                loading ?
+                    <>
+                        <CircularProgress />
+                    </>
+                    :
+                    <>
+                        <FormControl sx={{ m: 1, }} variant="standard">
+                            <TextField label="Username" variant="standard" name="username" value={login.username} onChange={handleChange} />
+                        </FormControl>
+                        <FormControl sx={{ m: 1, }} variant="standard">
+                            <InputLabel htmlFor="login-password">Password</InputLabel>
+                            <Input
+                                id="login-password"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                value={login.password}
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <Button variant="contained" sx={{ m: 1 }} color="success" onClick={handleUserSubmit}>Login</Button>
+                        <Button variant="contained" sx={{ m: 1 }} color="primary" onClick={handleGoogleSignIn} startIcon={<Google />}>Login with Google</Button>
+                        <Button variant="contained" sx={{
+                            m: 1,
+                            backgroundColor: '#4267B2'
+                        }} onClick={handleFacebookSignIn} startIcon={<FacebookRounded />}>Login with Facebook</Button>
+
+                    </>
+            }
 
         </Box>
     )
--- client/components/RegisterForm.jsx
+++ client/components/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { FacebookRounded, Google, Visibility, VisibilityOff } from "@mui/icons-material";
-import { FormControl, TextField, Input, InputLabel, InputAdornment, IconButton, Box, Button, Alert } from "@mui/material";
+import { FormControl, TextField, Input, InputLabel, InputAdornment, IconButton, Box, Button, Alert, CircularProgress } from "@mui/material";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import DataService from "../services/DataService";
@@ -15,6 +15,7 @@ const RegisterForm = () => {
         colour: ''
     });
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
 
     const handleChange = (event) => {
@@ -34,16 +35,19 @@ const RegisterForm = () => {
     };
 
     const handleUserSubmit = async (event) => {
+        setLoading(true);
         DataService.registerUser(register).then(res => {
             setMessage({
                 message: res.data.message,
                 colour: 'success'
             });
+            setLoading(false);
         }).catch(err => {
             setMessage({
                 message: err.response.data,
                 colour: 'error'
             });
+            setLoading(false);
         });
         setRegister({
             email: '',
@@ -64,40 +68,49 @@ const RegisterForm = () => {
         <Box sx={{ width: { xs: '95%', md: '40%' }, m: { xs: 1, md: 5 }, p: 2, display: 'grid', textAlign: 'center', boxShadow: 2 }}>
             {message.message !== '' && <Alert severity={message.colour}>{message.message}</Alert>}
             <h2>Create your free account</h2>
-            <FormControl sx={{ m: 1, }} variant="standard">
-                <TextField label="Email" variant="standard" name="email" value={register.email} onChange={handleChange} />
-            </FormControl>
-            <FormControl sx={{ m: 1, }} variant="standard">
-                <TextField label="Username" variant="standard" name="username" value={register.username} onChange={handleChange} />
-            </FormControl>
-            <FormControl sx={{ m: 1, }} variant="standard">
-                <InputLabel htmlFor="register-password">Password</InputLabel>
-                <Input
-                    id="register-password"
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    value={register.password}
-                    onChange={handleChange}
-                />
-            </FormControl>
-            <Button variant="contained" sx={{ m: 1 }} color="success" onClick={handleUserSubmit}>Register</Button>
-            <Button variant="contained" sx={{ m: 1 }} color="primary" onClick={handleGoogleSignUp} startIcon={<Google />}>Register with Google</Button>
-            <Button variant="contained" sx={{
-                m: 1,
-                backgroundColor: '#4267B2'
-            }} onClick={handleFacebookSignUp} startIcon={<FacebookRounded />}>Register with Facebook</Button>
+            {
+                loading ?
+                    <>
+                        <CircularProgress />
+                    </>
+                    :
+                    <>
+                        <FormControl sx={{ m: 1, }} variant="standard">
+                            <TextField label="Email" variant="standard" name="email" value={register.email} onChange={handleChange} />
+                        </FormControl>
+                        <FormControl sx={{ m: 1, }} variant="standard">
+                            <TextField label="Username" variant="standard" name="username" value={register.username} onChange={handleChange} />
+                        </FormControl>
+                        <FormControl sx={{ m: 1, }} variant="standard">
+                            <InputLabel htmlFor="register-password">Password</InputLabel>
+                            <Input
+                                id="register-password"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                value={register.password}
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <Button variant="contained" sx={{ m: 1 }} color="success" onClick={handleUserSubmit}>Register</Button>
+                        <Button variant="contained" sx={{ m: 1 }} color="primary" onClick={handleGoogleSignUp} startIcon={<Google />}>Register with Google</Button>
+                        <Button variant="contained" sx={{
+                            m: 1,
+                            backgroundColor: '#4267B2'
+                        }} onClick={handleFacebookSignUp} startIcon={<FacebookRounded />}>Register with Facebook</Button>
 
+                    </>
+            }
         </Box>
     );
 };
